8f028c278731ab25e5353bc7b163c9e39215797a
QianFan Jiang <bucherjiang@msn.com>
2020-10-03 04:14:02 +0800

    spi basic communication works

---
+++ src/ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WaveshareEInkDriver;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Is IsLittleEndian:{BitConverter.IsLittleEndian} ");
+            IT8951SPIDevice device = new IT8951SPIDevice(0, 0, 24);
+            var result = device.GetDeviceInfo();
+            Console.WriteLine($"{System.Text.Json.JsonSerializer.Serialize(result)},{result.Height},{result.Width}");
+            
+        }
+    }
+}
--- src/WaveshareEInkDriver/IT8951SPIDevice.cs
+++ src/WaveshareEInkDriver/IT8951SPIDevice.cs
@@ -8,6 +8,24 @@ using System.Threading;
 
 namespace WaveshareEInkDriver
 {
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Ansi, Size = 40)]
+    public struct DeviceInfo
+    {
+        [FieldOffset(0)]
+        public ushort Width;
+        [FieldOffset(2)]
+        public ushort Height;
+        [FieldOffset(4)]
+        public ushort bufferAddressL;
+        [FieldOffset(6)]
+        public ushort bufferAddressH;
+        [FieldOffset(8)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+        public string Version;
+        [FieldOffset(24)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+        public string LUTVersion;
+    }
     public class IT8951SPIDevice
     {
         SpiDevice spi;
@@ -15,19 +33,24 @@ namespace WaveshareEInkDriver
         private const int MAX_SEND_BUFFER_SIZE = 2048;
         private const int MAX_RECEIVE_BUFFER_SIZE = 2048;
 
-        private byte[] sendDataBuffer = new byte[MAX_SEND_BUFFER_SIZE+2];    
-        private byte[] receiveDataBuffer = new byte[MAX_RECEIVE_BUFFER_SIZE+2]; 
+        private byte[] sendDataBuffer = new byte[MAX_SEND_BUFFER_SIZE + 2];
+        private byte[] receiveDataBuffer = new byte[MAX_RECEIVE_BUFFER_SIZE + 2];
         private byte[] sendCommandBuffer = new byte[4] { 0x60, 0x00, 0x00, 0x00 };
         private readonly byte[] receiveDataPrebeam = new byte[2] { 0x10, 0x00 };
-        
+
 
         public IT8951SPIDevice(int spiDeviceId, int CSPin, int HRDYpin)
         {
             GpioController gpioController = new GpioController();
+            gpioController.OpenPin(HRDYpin, PinMode.Input);
             gpioController.RegisterCallbackForPinValueChangedEvent(HRDYpin, PinEventTypes.Rising, onDeviceReady);
             gpioController.RegisterCallbackForPinValueChangedEvent(HRDYpin, PinEventTypes.Falling, onDeviceBusy);
+
+            gpioController.OpenPin(17, PinMode.Output);
+            gpioController.Write(17, PinValue.High);
+
             SpiConnectionSettings settings = new SpiConnectionSettings(spiDeviceId, CSPin);
-            settings.ClockFrequency = 12000000;
+            settings.ClockFrequency = 2000000; //RP4 must use 2M clock
             settings.Mode = SpiMode.Mode0;
             settings.ChipSelectLineActiveState = PinValue.Low;
             settings.DataFlow = DataFlow.MsbFirst;
@@ -36,11 +59,13 @@ namespace WaveshareEInkDriver
 
         private void onDeviceReady(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
         {
+            Console.WriteLine("HRDY-High");
             mre.Set();
         }
 
         private void onDeviceBusy(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
         {
+            Console.WriteLine("HRDY-Low");
             mre.Reset();
         }
 
@@ -77,15 +102,24 @@ namespace WaveshareEInkDriver
             return readData<byte>();
         }
 
+        public DeviceInfo GetDeviceInfo()
+        {
+            sendCommand(0x0203); //BUGBUG: need to add reverse byte order for words
+            return readData<DeviceInfo>();
+        }
+
+
+
 
         private void sendCommand(ushort cmd)
         {
             MemoryMarshal.Write(sendCommandBuffer.AsSpan(2, 2), ref cmd);
             mre.WaitOne();
+            Console.WriteLine($"SPI Send:{BitConverter.ToString(sendCommandBuffer)}");
             spi.Write(sendCommandBuffer);
         }
 
-        private void sendData<T>(T data) where T:struct
+        private void sendData<T>(T data) where T : struct
         {
             var buffer = MemoryMarshal.AsBytes<T>(new T[] { data });
             sendData(buffer);
@@ -95,41 +129,64 @@ namespace WaveshareEInkDriver
         {
             int size = data.Length;
             int pos = 0;
-            while (size>0)
+            Console.Write("SPI Send:");
+            while (size > 0)
             {
                 int sendSize = Math.Min(MAX_SEND_BUFFER_SIZE, size);
                 var sendBuffer = sendDataBuffer.AsSpan(0, sendSize + 2);
-                data.Slice(pos,sendSize).CopyTo(sendBuffer.Slice(2, sendSize));
+                data.Slice(pos, sendSize).CopyTo(sendBuffer.Slice(2, sendSize));
                 pos += sendSize;
                 size -= sendSize;
                 mre.WaitOne();
                 spi.Write(sendBuffer);
+                Console.Write(BitConverter.ToString(sendBuffer.ToArray()));
             }
-            
+            Console.WriteLine();
+
         }
 
         private void readData(Span<byte> buffer)
         {
             int size = buffer.Length;
             int pos = 0;
-            while (size>0)
+            while (size > 0)
             {
                 int receiveSize = Math.Min(size, MAX_RECEIVE_BUFFER_SIZE);
                 var receiveBuffer = receiveDataBuffer.AsSpan(0, receiveSize + 2);
-                receiveDataPrebeam.CopyTo(receiveBuffer.Slice(0, 2));
+                Span<byte> sendBuffer = new byte[receiveSize + 2];
+                receiveDataPrebeam.CopyTo(sendBuffer.Slice(0, 2));
                 mre.WaitOne();
-                spi.TransferFullDuplex(receiveBuffer, receiveBuffer);
-                receiveBuffer.Slice(2, buffer.Length).CopyTo(buffer.Slice(0,receiveSize));
+                receiveBuffer.Fill(0);
+                spi.TransferFullDuplex(sendBuffer, receiveBuffer);
+                receiveBuffer.Slice(2, receiveSize).CopyTo(buffer.Slice(pos, receiveSize));
                 pos += receiveSize;
                 size -= receiveSize;
+                Console.WriteLine($"SPI Receive [{receiveBuffer.Length}]:{BitConverter.ToString(receiveBuffer.ToArray())}");
             }
         }
 
-        private T readData<T>()where T:struct
+        private T readData<T>() where T : struct
         {
             byte[] buffer = new byte[Marshal.SizeOf<T>()];
             readData(buffer);
-            return MemoryMarshal.Read<T>(buffer);
+            Console.WriteLine($"data read[{buffer.Length}]:  {BitConverter.ToString(buffer)}");
+            var handle = GCHandle.Alloc(buffer,GCHandleType.Pinned);
+            try
+            {
+                var result = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+                return result;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            finally
+            {
+                handle.Free();
+            }
+            
+            //return MemoryMarshal.Read<T>(buffer);
         }
     }
 
